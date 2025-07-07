74479b6716bd4552164d4ca47809837fa4fc5e51
Tqngoc <qyhng.mg@gmail.com>
2025-07-07 09:26:37 +0700

    Triangle number exercise

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Chicken
+{
+    class NollowaChicken
+    {
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("Input the number you like");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the width");
+            int totalColumn = Convert.ToInt32(Console.ReadLine());
+
+            int totalRow = totalColumn;
+            for (int row = 0; row < totalRow; row++)
+            {
+                for (int col = 0; col < totalColumn; col++)
+                {
+                    Console.Write(num);
+                }
+                Console.WriteLine();
+                totalColumn--;
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
